17c6f38de26fdaedeadab3f336b0e3c1404d2a88
UneebSidd <Uneeb.Siddiqui27@qmail.cuny.edu>
2022-07-19 14:53:46 -0500

    dataExtractor class added

--- DataGenerator.java
+++ DataGenerator.java
@@ -10,14 +10,13 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 
-/* Class to demonstrate the use of Spreadsheet Write and Read API */
+/* Class to demonstrate the use of Spreadsheet Write API */
 public class DataGenerator {
 
 
@@ -64,8 +63,9 @@ public class DataGenerator {
         return result;
     }
 
-    public static  String randomNumericStrings(int length)
+    public String randomNumericStrings(int length)
     {
+        //string of all numbers 0-9
         String numbers = "0123456789";
 
         StringBuilder stringBuilder = new StringBuilder();
@@ -81,17 +81,23 @@ public class DataGenerator {
         return stringBuilder.toString();
     }
 
-    public static String randomStates()
-    {
-        String[] countries = {"New York","Texas",
-                               "Boston","California", "Arizona",
-                                 "Arkansas", "Florida", "Virginia", "Alaska", "Washington"};
+    public  String randomStates() throws FileNotFoundException {
+
+        ArrayList<String> states = new ArrayList<>() ;
+        File myFile = new File("/Users/uneebsiddiqui/Desktop/OAS/Java/src/main/resources/states.txt");
+        Scanner fileReader = new Scanner(myFile);
+        while(fileReader.hasNextLine())
+        {
+            states.add(fileReader.nextLine());
+        }
+
+
         Random random = new Random();
-        int randomCountry = random.nextInt(countries.length);
-        return countries[randomCountry];
+        int randomCountry = random.nextInt(states.size());
+        return states.get(randomCountry);
     }
 
-    public static String randomStrings()
+    public String randomStrings()
     {
         String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
@@ -118,9 +124,12 @@ public class DataGenerator {
         return sb.toString();
     }
 
-    public static List<Object> dataGeneration()
-    {
+
+
+    public  List<Object> dataGeneration() throws FileNotFoundException {
+
         List<Object> list = new ArrayList<Object>();
+
 
 
         String firstName = randomStrings();
@@ -138,14 +147,18 @@ public class DataGenerator {
         return list;
     }
 
-    public static List<List<Object>> addDataToList()
-    {
+
+
+
+    public  List<List<Object>> addDataToList() throws FileNotFoundException {
         List<List<Object>>  theList = new ArrayList<>();
-        for(int i=0; i<10; i++)
+        for(int i=0; i<10000; i++)
         {
-            List<Object> list = new ArrayList<Object>();
-            list = dataGeneration();
+            List<Object> list = dataGeneration();
+
+
             theList.add(list);
+
         }
 
         return theList;
@@ -153,151 +166,3 @@ public class DataGenerator {
 
 
 }
-
-
-
-/*
-
-
-            import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
-import com.google.api.services.sheets.v4.model.Spreadsheet;
-import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
-import com.google.auth.http.HttpCredentialsAdapter;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.api.client.googleapis.json.GoogleJsonError;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
-import com.google.api.services.sheets.v4.model.ValueRange;
-
-
-
-import java.io.IOException;
-import java.util.*;
-*/
-
-/* Class to demonstrate the use of Spreadsheet Create API */
-/*
-public class HelloWorld {
-    /**
-     * Create a new spreadsheet.
-     *
-     * @param title - the name of the sheet to be created.
-     * @return newly created spreadsheet id
-     * @throws IOException - if credentials file not found.
-     */
-
-
-/*
-    public static String createSpreadsheet(String title) throws IOException {
-        /* Load pre-authorized user credentials from the environment.
-           TODO(developer) - See https://developers.google.com/identity for
-            guides on implementing OAuth2 for your application. */
-
-            /*
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
-                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
-                credentials);
-
-        // Create the sheets API client
-        Sheets service = new Sheets.Builder(new NetHttpTransport(),
-                GsonFactory.getDefaultInstance(),
-                requestInitializer)
-                .setApplicationName("Sheets samples")
-                .build();
-
-        // Create new spreadsheet with a title
-        Spreadsheet spreadsheet = new Spreadsheet()
-                .setProperties(new SpreadsheetProperties()
-                        .setTitle(title));
-        spreadsheet = service.spreadsheets().create(spreadsheet)
-                .setFields("spreadsheetId")
-                .execute();
-
-        // Prints the new spreadsheet id
-        System.out.println("Spreadsheet ID: " + spreadsheet.getSpreadsheetId());
-        return spreadsheet.getSpreadsheetId();
-    }
-
-    public static UpdateValuesResponse updateValues(String spreadsheetId,
-                                                    String range,
-                                                    String valueInputOption,
-                                                    List<List<Object>> values)
-
-            throws IOException {
-        /* Load pre-authorized user credentials from the environment.
-           TODO(developer) - See https://developers.google.com/identity for
-            guides on implementing OAuth2 for your application. */
-
-    /*
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
-                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
-                credentials);
-
-        // Create the sheets API client
-        Sheets service = new Sheets.Builder(new NetHttpTransport(),
-                GsonFactory.getDefaultInstance(),
-                requestInitializer)
-                .setApplicationName("Sheets samples")
-                .build();
-
-        UpdateValuesResponse result =null;
-        try {
-            // Updates the values in the specified range.
-            ValueRange body = new ValueRange()
-                    .setValues(values);
-            result = service.spreadsheets().values().update(spreadsheetId, range, body)
-                    .setValueInputOption(valueInputOption)
-                    .execute();
-            System.out.printf("%d cells updated.", result.getUpdatedCells());
-        } catch (GoogleJsonResponseException e) {
-            // TODO(developer) - handle error appropriately
-            GoogleJsonError error = e.getDetails();
-            if (error.getCode() == 404) {
-                System.out.printf("Spreadsheet not found with id '%s'.\n",spreadsheetId);
-            } else {
-                throw e;
-            }
-        }
-        return result;
-    }
-
-
-    public static List<Object> dataGeneration(String firstName,String lastName, String email,String state, String city, String zip)
-    {
-        List<Object> list = new ArrayList<Object>();
-        list.add(firstName);
-        list.add(lastName);
-        list.add(email);
-        list.add(state);
-        list.add(city);
-        list.add(zip);
-
-        return list;
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        System.out.println("hello");
-        String id = "13obkoMGF_gwWf3VNMg06f4c4-zXhRmLq0E7p3YLCBQI";
-        List<List<Object>> aList = new LinkedList<List<Object>>();
-        List<Object> another = dataGeneration("Uneeb", "Siddiqui", "uu1997@gmail.com", "NY", "Queens","11432");
-        aList.add(another);
-
-        updateValues(id,"Sheet1!A2:F","RAW",aList);
-
-
-
-    }
-}
-
-
-
-
-*/
--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         //instantiate CreateSheet object
         CreateSheet sheet2 = new CreateSheet();
@@ -14,13 +14,40 @@ public class Main {
         //instantiate DataGenerator class object
         DataGenerator dataGenerator = new DataGenerator();
 
+
+
         //stores the newly created sheet id in variable named 'id'.
-        String id = sheet2.createSpreadsheet("Sheet2");
+        String id = sheet2.createSpreadsheet("Sheet");
 
-        List<List<Object>> aList = new LinkedList<List<Object>>();
-        aList = dataGenerator.addDataToList();
+        //writing newly created sheet's id to a file to keep track of the sheets.
 
-        dataGenerator.updateValues(id,"Sheet1!A2:F","RAW",aList);
+        try {
+            FileWriter fileWriter = new FileWriter("/Users/uneebsiddiqui/Desktop/OAS/Java/src/main/resources/log.txt");
+            fileWriter.write("Sheet ID " + id);
+            fileWriter.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Can not write to log file.");
+        }
+
+        //loading data to List of list
+
+       List<List<Object>> aList = dataGenerator.addDataToList();
+
+        //uploading data to google sheet
+        dataGenerator.updateValues(id,"Sheet1!A2:Z","RAW",aList);
+
+        CreateSheet.shareFile(id,"uu1997@gmail.com", "@gmail.com");
+
+
+       List<List<Object>>  data = DataExtractor.getData(id,"Sheet1", "A2:Z");
+
+
+
+
+
 
     }
 }
+
