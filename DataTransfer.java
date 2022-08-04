70cbdb61900879736a5ccf6f4c5586d1bef095a5
UneebSidd <Uneeb.Siddiqui27@qmail.cuny.edu>
2022-08-04 15:34:42 -0400

    dataTransfer

---
+++ DataTransfer.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataTransfer {
+
+    public static void dataConnection() throws SQLException {
+        Connection connection = null;
+
+
+        try {
+
+            String url = "jdbc:mysql://localhost:3306/sys";
+            String user = "root";
+            String password = "Yaqoob50";
+
+            connection = DriverManager.getConnection(url, user, password);
+            if (connection != null) {
+                System.out.println("Connected");
+            }
+
+
+        }
+        catch (SQLException  e)
+        {
+            System.out.println("password or user invalid");
+            e.printStackTrace();
+        }
+    }
+}
